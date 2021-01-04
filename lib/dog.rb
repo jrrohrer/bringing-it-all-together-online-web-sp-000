89222f74f692e93f3cdfc39c9f7b7e81b8edae11
Jessica Rohrer <jessie.rohrer@gmail.com>
2021-01-04 01:41:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,8 +40,8 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
   
-  def self.create(name:, breed:)
-    dog = Dog.new(name, breed)
+  def self.create(attr_hash)
+    dog = Dog.new(attr_hash)
     dog.save
     dog
   end
